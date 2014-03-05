82be3a4badd9c7c895b68459418074d0dc2bb610
rassaoui <rassaoui.nidhal@gmail.com>
2014-03-05 11:11:15 +0100

    Model d'utilisation du passage d'options "Short Open Tag"

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,20 @@
 
 module.exports = function(grunt) {
 	var cfg = {
-		jshint: {
-			options: {
-				boss: true,
-				node: true
-			},
-
-			all: ["tasks/**/*.js", "test/**/*.js", "Gruntfile.js"]
-		},
-
-		simplemocha: {
-			options: {
-				reporter: "spec",
-				ui: "bdd",
-				compilers: "coffee:coffee-script"
-			},
-
-			all: ["test/*_spec.js"]
-		},
-
+		
 		phplint: {
-			good: ["test/rsrc/*-good.php"],
-			good_nocache: {
-				options: {
-					cache: false
-				},
-				files: {
-					src: ["test/rsrc/*-good.php"]
-				}
-			},
-			bad: ["test/rsrc/*-fail.php"],
+			good: ["*ShortOpenTag_good.php"],
+			
+			bad: ["*ShortOpenTag_bad.php"],
 
-			explicit: {
-				options: {
-					phpCmd: "/usr/bin/php"
-				},
-
-				src: ["test/rsrc/*-good.php"]
-			}
+			
 		}
 	};
 
 	grunt.initConfig(cfg);
 
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-simple-mocha");
-	grunt.loadTasks("./tasks");
 
-	grunt.registerTask("default", ["jshint:all", "simplemocha:all", "phplint:good", "phplint:good_nocache"]);
+	grunt.loadNpmTasks('grunt-phplint');
+
+	grunt.registerTask("default", ["phplint:good", "phplint:good_nocache"]);
 };
